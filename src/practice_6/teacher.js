7cd019fd3ef0d54514df8561e6435694bcf9b0be
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-04 16:23:20 +0800

    Finished practice 6

--- src/practice_6/teacher.js
+++ src/practice_6/teacher.js
@@ -1,1 +1,17 @@
+import Person from './person';
 
+class Worker extends Person {
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    introduce() {
+        if(this.klass){
+            return super.introduce() + ` I am a Teacher. I teach Class ${this.klass}.`
+        }
+        return super.introduce() + ` I am a Teacher. I teach No Class.`
+    }
+}
+
+export default Worker;
