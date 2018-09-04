3e0b44e8289e42b908dea5f4a2680fc10e924fc3
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-04 16:43:30 +0800

    Finished practice 8

--- src/practice_8/class.js
+++ src/practice_8/class.js
@@ -1,1 +1,18 @@
+class Class{
+    constructor(no) {
+        this.number = no;
+    }
 
+    getDisplayName() {
+        return `Class ${this.number}`;
+    }
+
+    assignLeader(student) {
+        if (student.klass.number !== this.number) {
+            return ;
+        }
+        this.leader = student;
+    }
+}
+
+export default Class;
