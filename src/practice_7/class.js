af2d16c3e8fa335bfcca04808361e1ab95b32d1a
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-04 16:32:33 +0800

    Finished practice 7

--- src/practice_7/class.js
+++ src/practice_7/class.js
@@ -1,1 +1,11 @@
+class Class{
+    constructor(no) {
+        this.number = no;
+    }
 
+    getDisplayName() {
+        return `Class ${this.number}`;
+    }
+}
+
+export default Class;
--- src/practice_7/teacher-option2.js
+++ src/practice_7/teacher-option2.js
@@ -1,1 +1,25 @@
+import Person from './person';
+
+class Worker extends Person {
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    introduce() {
+        if(this.klass){
+            return super.introduce() + ` I am a Teacher. I teach Class ${this.klass.number}.`
+        }
+        return super.introduce() + ` I am a Teacher. I teach No Class.`
+    }
+
+    introduceWith(student) {
+        if (student.klass.number === this.klass.number) {
+            return `My name is ${this.name}. I am ${this.age} years old. I am a Teacher. I teach ${student.name}.`;
+        }
+        return `My name is ${this.name}. I am ${this.age} years old. I am a Teacher. I don't teach ${student.name}.`;
+    }
+}
+
+export default Worker;
 
