5d902b4d751ee0f9888f83f2ad84809285e1a2d3
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-04 16:14:26 +0800

    Change not to just import and then export

--- src/practice_2/person.js
+++ src/practice_2/person.js
@@ -1,3 +1,12 @@
-import Person from '../practice_1/person'
+class Person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    introduce() {
+        return `My name is ${this.name}. I am ${this.age} years old.`;
+    }
+}
 
 export default Person;
--- src/practice_3/student.js
+++ src/practice_3/student.js
@@ -1,3 +1,14 @@
-import Student from '../practice_2/student';
+import Person from './person'
+
+class Student extends Person {
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    introduce() {
+        return `I am a Student. I am at Class ${this.klass}.`;
+    }
+}
 
 export default Student;
