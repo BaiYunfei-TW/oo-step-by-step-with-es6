cd55d935f2cd4ed5e368e94f351ffb1639e97de1
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-04 16:07:17 +0800

    Finished practice 2

--- src/practice_2/student.js
+++ src/practice_2/student.js
@@ -1,1 +1,14 @@
+import Person from './person'
 
+class Student extends Person {
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    introduce() {
+        return `My name is ${this.name}. I am ${this.age} years old.`;
+    }
+}
+
+export default Student;
